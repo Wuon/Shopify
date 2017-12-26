9fa8e4f2494c0e5ecaaa9f86970ab05aee039e41
Wuon <wuonlabs@gmail.com>
2017-12-26 16:28:52 -0500

    1.0.0

---
+++ Frontend_Challenge/main.js
@@ -0,0 +1,24 @@
+function myFunction() { 
+	if (emailAddress.value == null || emailAddress.value == "" || document.getElementById("interestDropdown").options.selectedIndex == 0){
+		document.getElementById("errorMessage").textContent = "Please fill out the required fields"; 
+		document.getElementById("errorMessage").style.visibility = "visible"; 
+	}else{
+		validity = validateEmail(emailAddress.value)
+	    if (validity){
+	    	console.log(emailAddress.value)
+	    	console.log(interestDropdown.value)
+	    }else{
+	    	document.getElementById("errorMessage").textContent = "Please enter a valid email address"; 
+	    	document.getElementById("errorMessage").style.visibility = "visible"; 
+	    }
+	}
+}
+
+function validateEmail(email){
+    var re = /\S+@\S+/;
+    return re.test(email);
+}
+
+function removeText(){
+	document.getElementById("errorMessage").style.visibility = "hidden"; 
+}
